2181b810d9e8bfc251b8d847854a82275f98d230
Niriantsoa Herge <niriantsoaherge@gmail.com>
2024-10-11 12:42:56 +0300

    update modal

--- Home/js/contact.js
+++ Home/js/contact.js
@@ -1,22 +1,35 @@
-
 let modal = document.getElementById('myModal');
-// Quand l'utilisateur clique sur le bouton, ouvrez la boîte de dialogue si le formulaire est rempli
-let btn = document.getElementById('button').addEventListener('click', () => {
-    let input = document.querySelectorAll('input');
-    if (input.value =='' || input.value == null) {
+let form = document.querySelector('form');
+let input = document.querySelector('form input');
+let btn = document.getElementById('button');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    submit();
+});
+
+btn.addEventListener('click', envoyer);
+function envoyer() {
+    
+    if (input.value == '' || input.value == null) {
         modal.style.display = "none";
+        console.log('remplir')
     } else {
         modal.style.display = 'block';
-    }
-});
+        input.value = '';
+        input.required = false;
+    }  
+    /*
+   modal.style.display = 'block';
+   */
+    
+};
 
 
 // Quand l'utilisateur clique sur le "x", fermez la boîte de dialogue
 let span = document.getElementsByClassName("close")[0].addEventListener('click', () => {
    modal.style.display = "none";
 });
-
-
 
 
 // Quand l'utilisateur clique en dehors de la boîte de dialogue, fermez-la
